3219d894bb14c0c520b085446fc9096771f60bc2
kaiwalya <dani.kaiwalya@gmail.com>
2017-02-10 15:41:53 +0530

    first commit

---
+++ TestNew.java
@@ -0,0 +1,40 @@
+class A {
+	
+	A() {
+		System.out.println("In A const");
+	}
+	void show() {
+		System.out.println("A");
+	}
+}
+
+class B  extends A{
+
+	B() {
+		System.out.println("In B const");
+	}
+	void show() {
+		System.out.println("B");
+			//super.show();
+		System.out.println("B");
+	}
+}
+
+class C  extends B{
+
+	C() {
+		System.out.println("In C const");
+	}
+	void show() {
+		System.out.println("B");
+			//super.show();
+		System.out.println("B");
+	}
+}
+
+class TestNew {
+	public static void main(String a[]) {
+		A obj = new A(2,"sra");
+		obj.show();
+	}
+}
